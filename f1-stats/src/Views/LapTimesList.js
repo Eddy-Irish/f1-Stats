631881aac0f0bd0ef769ad8fcee46db6e7136342
Eddy-Irish <keeganohara7@gmail.com>
2021-07-27 21:18:05 -0400

    Changing to use Mongo Realm App, & ApolloProvider

--- f1-stats/src/Views/LapTimesList.js
+++ f1-stats/src/Views/LapTimesList.js
@@ -1,73 +1,29 @@
 import React, { Component } from "react";
-import axios from 'axios';
-// import { Link } from "react-router-dom";
+import { gql, useQuery } from '@apollo/client';
 
-const LapTime = (props) => (
-  <tr>
-    <td>{props.LapTime.location}</td>
-    <td>{props.LapTime.year}</td>
-    <td>{props.LapTime.pole}</td>
-    <td>{props.LapTime.fastestLap}</td>
-    {/* <td> */}
-      {/* <Link to={"/edit/" + props.LapTime._id}>Edit</Link> |
-      <a
-        href="/"
-        onClick={() => {
-          props.deleteRecord(props.record._id);
-        }}
-      >
-        Delete
-      </a> */}
-    {/* </td> */}
-  </tr>
-);
 
-export default class LapTimesList extends Component {
-  // This is the constructor that shall store our data retrieved from the database
-  constructor(props) {
-    super(props);
-    // this.deleteRecord = this.deleteRecord.bind(this);
-    this.state = { LapTimes: [] };
+const GET_LAPS = gql`
+  query GetLaps {
+    lapTimes {
+      _id
+      fastestLap
+      location
+      pole
+      year
+    }
   }
+`;
 
-  // This method will get the data from the database.
-  componentDidMount() {
-    axios
-      .get("http://localhost:3000/F1-Data/LapTimes/find")
-      .then((response) => {
-        this.setState({ LapTimes: response.data });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+export default function LapTimesList() {
 
-  // This method will delete a record based on the method
-  // deleteLapTime(id) {
-  //   axios.delete("http://localhost:3000/" + id).then((response) => {
-  //     console.log(response.data);
-  //   });
+  const { loading, error, data } = useQuery(GET_LAPS);
+    if(loading) {
+      return <div>LOADING...</div>
+    }
+    if(error){
+      return <div>encountered and error, dummy: {error}</div>
+    }
 
-  //   this.setState({
-  //     LapTime: this.state.LapTimes.filter((el) => el._id !== id),
-  //   });
-  // }
-
-  // This method will map out the users on the table
-  LapTimesList() {
-    return this.state.LapTimes.map((currentlaptime) => {
-      return (
-        <LapTime
-          LapTime={currentlaptime}
-          // deleteLapTime={this.deleteLapTime}
-          key={currentlaptime._id}
-        />
-      );
-    });
-  }
-
-  // This following section will display the table with the records of individuals.
-  render() {
     return (
       <div>
         <h3>Historical Pole and Fastest Lap Times From the Monaco Grand Prix</h3>
@@ -79,10 +35,18 @@ export default class LapTimesList extends Component {
               <th>Pole</th>
               <th>Fastest Lap</th>
             </tr>
+            <tr>
+              {data.lapTimes.map(lapTime => (
+                <th>{lapTime.location}
+                    {lapTime.year}
+                    {lapTime.pole}
+                    {lapTime.fastestLap}
+                </th>
+              ))}
+            </tr>
           </thead>
-          <tbody>{this.LapTimesList()}</tbody>
+          <tbody></tbody>
         </table>
       </div>
     );
-  }
 }
--- f1-stats/src/index.js
+++ f1-stats/src/index.js
@@ -3,11 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Realm from 'realm-web';
+import { ApolloClient,
+         ApolloProvider,
+         HttpLink,
+         InMemoryCache } from '@apollo/client';
+
+export const APP_ID = "f1-stats-mongo-realm-fkrpb";
+const graphqlUri = `https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/f1-stats-mongo-realm-fkrpb/graphql`
+
+// Connect to MongoDB Realm App
+const app = new Realm.App(APP_ID);
+
+// Gets a valid Realm user access token to authenticate requests
+async function getValidAccessToken() {
+  // Guarantee that there is a logged in user with a valid access token
+  if (!app.currentUser) {
+    await app.logIn(Realm.Credentials.anonymous());
+  } else {
+    // An already logged in user's access toekn might be stale, to guarantee that the token
+    // is valid, refresh the user's custom data which also refreshes the access token
+    await app.currentUser.refreshCustomData();
+  }
+  return app.currentUser.accessToken
+}
+
+// Configure the ApolloClient to connect to your app's GraphQL endpoint
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: graphqlUri,
+    // Define a custom fetch handler for ApolloClient that authenticate GraphQL requests
+    // Function intercepts every Apollo HTTP request and adds an Authorization header with a
+    // valid access token before sending the request
+    fetch: async (uri, options) => {
+      const accessToken = await getValidAccessToken();
+      options.headers.Authorization = `Bearer ${accessToken}`;
+      return fetch(uri, options);
+    },
+  }),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
